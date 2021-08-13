13d12414468b5c04c366fea6c3ab58cbddddfc28
Cervantes007 <lmartellguevara@gmail.com>
2021-08-13 12:21:26 -0400

    fix(pipe): rename cors pipe to $cors

--- templates/typescript/src/app.ts
+++ templates/typescript/src/app.ts
@@ -1,7 +1,7 @@
-import { App, cors } from 'wind-waker';
+import { App, $cors } from 'wind-waker';
 
 const app = new App({ ui: process.env.NODE_ENV === 'development' });
 
-app.pipeline(cors({ origin: 'http://localhost:8090' }));
+app.pipeline($cors({ origin: 'http://localhost:8090' }));
 
 export { app };
