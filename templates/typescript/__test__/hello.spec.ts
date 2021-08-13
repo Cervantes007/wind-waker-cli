b3d0329077a4ebee04ca211fcfcd398bd1fe11e9
Cervantes007 <lmartellguevara@gmail.com>
2021-08-13 11:54:12 -0400

    feat(template): update template to include ui and gitignore files

--- templates/typescript/__test__/hello.spec.ts
+++ templates/typescript/__test__/hello.spec.ts
@@ -2,14 +2,14 @@ import { request } from './client';
 
 describe('hello Action', () => {
   test('testing hello GET action', async () => {
-    const name = 'Test';
+    const name = 'Test GET';
     const response = await request('hello', { name }, { method: 'GET' });
     expect(response.status).toBe(200);
     expect(response.data).toBe(`Hello ${name}!`);
   });
 
   test('testing hello POST action', async () => {
-    const name = 'Test';
+    const name = 'Test POST';
     const response = await request('hello', { name });
     expect(response.status).toBe(200);
     expect(response.data).toBe(`Hello ${name}!`);
